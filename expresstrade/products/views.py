6af2c5001f229f73881d115b2ad33b0283536acf
Sergiusz <codeimperium@gmail.com>
2018-01-14 20:57:59 +0100

    static configuration

---
+++ expresstrade/products/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.views.generic.list import ListView
+from .models import Product
+
+
+# Create your views here.
+
+class ProductListView(ListView):
+    queryset = Product.objects.all()
+
+
+def product_list_view(request):
+    queryset = Product.objects.all()
+    context = {
+        'object_list': queryset
+    }
+    return render(request, "product/product_list_view.html", context)
